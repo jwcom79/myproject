f9173b2a5cb96bd4421bb3eb465c9ded449a506d
jwcom79 <mak_flathead@naver.com>
2015-12-22 16:19:47 +0900

    pushin_layer added

--- src/caffe/layers/pushin_layer.cpp
+++ src/caffe/layers/pushin_layer.cpp
@@ -14,6 +14,7 @@ namespace caffe{
 				DCHECK(threshold_ < 1.);
 				scale_ = 1. / (1. - threshold_);
 				uint_thres_ = static_cast<unsigned int>(UINT_MAX * threshold_);
+				st_count = bottom[0]->count() * threshold_;
 		}
 
 		template <typename Dtype>
@@ -32,15 +33,36 @@ namespace caffe{
 				Dtype* top_data = top[0]->mutable_cpu_data();
 				unsigned int* mask = rand_vec_.mutable_cpu_data();
 				const int count = bottom[0]->count();
-				if (this->phase_ == TRAIN) {
+				int st_point = bottom[0]->count();
+				st_point *= threshold_;
+				//st_count = this->layer_param_.pushin_param().st_count();
+
+				//if (this->phase_ == TRAIN) {
 						// Create random numbers
+						/*
 						caffe_rng_bernoulli(count, 1. - threshold_, mask);
 						for (int i = 0; i < count; ++i) {
 								top_data[i] = bottom_data[i] * mask[i] * scale_;
 						}
-				} else {
-						caffe_copy(bottom[0]->count(), bottom_data, top_data);
-				}
+						*/
+						for (int i = 0; i < st_count; ++i) {
+								mask[i] = 1;
+						}
+						for (int i = st_count; i < count; ++i) {
+								mask[i] = 0;
+						}
+						for (int i = 0; i < count; ++i) {
+								top_data[i] = bottom_data[i] * mask[i] * scale_;
+						}
+				//} else {
+						//caffe_copy(bottom[0]->count(), bottom_data, top_data);
+				//		for (int i = 0; i < count; ++i) {
+				//				top_data[i] = bottom_data[i] * mask[i] * scale_;
+				//		}
+				//}
+				st_count++;
+				if (st_count > count)
+						st_count -= 1;
 		}
 
 
@@ -48,19 +70,26 @@ namespace caffe{
 		void PushinLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
 																					const vector<bool>& propagate_down,
 																					const vector<Blob<Dtype>*>& bottom){
-				if (propagate_down[0]) {
+				//if (propagate_down[0]) {
 						const Dtype* top_diff = top[0]->cpu_diff();
 						Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
-						if (this->phase_ == TRAIN) {
-								const unsigned int* mask = rand_vec_.cpu_data();
-								const int count = bottom[0]->count();
+				unsigned int* mask = rand_vec_.mutable_cpu_data();
+				const int count = bottom[0]->count();
+						//if (this->phase_ == TRAIN) {
+						//		const unsigned int* mask = rand_vec_.cpu_data();
+						for (int i = 0; i < st_count; ++i) {
+								mask[i] = 1;
+						}
+						for (int i = st_count; i < count; ++i) {
+								mask[i] = 0;
+						}
 								for (int i = 0; i < count; ++i) {
 										bottom_diff[i] = top_diff[i] * mask[i] * scale_;
 								}
-						} else {
-								caffe_copy(top[0]->count(), top_diff, bottom_diff);
-						}
-				}
+						//} else {
+						//		caffe_copy(top[0]->count(), top_diff, bottom_diff);
+						//}
+				//}
 		}
 
 
